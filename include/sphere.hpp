062243c39386588a1ebde50b300ec99d72a411c6
Kaan Eraslan <kaaneraslan@gmail.com>
2020-06-19 07:50:03 +0200

    Revert "sphere modified"
    This reverts commit dee6f94b4fda79ff68f4abb1629bf504b72e828e.

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -9,8 +9,6 @@
 //
 #include <material.hpp>
 //
-#include <onb.hpp>
-//
 class Sphere : public Hittable {
 public:
   point3 center;
@@ -22,7 +20,7 @@ public:
   Sphere(point3 cent, double r, shared_ptr<Material> mat)
       : center(cent), radius(r), mat_ptr(mat){};
   bool hit(const Ray &r, double dist_min, double dist_max,
-           HitRecord &record) const override {
+           HitRecord &record) const {
     // kureye isin vurdu mu onu test eden fonksiyon
     vec3 origin_to_center = r.origin - center;
     double a = dot(r.direction, r.direction);
@@ -38,8 +36,15 @@ public:
         record.point = r.at(record.dist);
         vec3 out_normal = (record.point - center) / radius;
         record.set_face_normal(r, out_normal);
-        vec3 uv_normal = record.front_face ? record.normal : -1 * record.normal;
-        get_sphere_uv(uv_normal, record.u, record.v);
+        if (record.front_face) {
+          // ray is outside of the sphere
+          // the normal points from center to outside
+          get_sphere_uv(record.normal, record.u, record.v);
+        } else {
+          // ray is inside of the sphere
+          // the normal points from inside to center
+          get_sphere_uv(-record.normal, record.u, record.v);
+        }
         // get_sphere_uv((record.point - center) / radius, record.u, record.v);
         record.mat_ptr = mat_ptr;
         return true;
@@ -50,35 +55,26 @@ public:
         record.point = r.at(record.dist);
         vec3 out_normal = (record.point - center) / radius;
         record.set_face_normal(r, out_normal);
-        vec3 uv_normal = record.front_face ? record.normal : -1 * record.normal;
-        get_sphere_uv(uv_normal, record.u, record.v);
+        if (record.front_face) {
+          // ray is outside of the sphere
+          // the normal points from center to outside
+          get_sphere_uv(record.normal, record.u, record.v);
+        } else {
+          // ray is inside of the sphere
+          // the normal points from inside to center
+          get_sphere_uv(-record.normal, record.u, record.v);
+        }
+        // get_sphere_uv((record.point - center) / radius, record.u, record.v);
         record.mat_ptr = mat_ptr;
         return true;
       }
     }
     return false;
   }
-  bool bounding_box(double t0, double t1, Aabb &output_bbox) const override {
+  bool bounding_box(double t0, double t1, Aabb &output_bbox) const {
     //
     output_bbox = Aabb(center - vec3(radius), center + vec3(radius));
     return true;
-  }
-  double pdf_value(const point3 &p, const vec3 &v) const override {
-    HitRecord rec;
-    if (this->hit(Ray(p, v), 0.001, INF, rec) == false) {
-      return 0;
-    }
-    vec3 center_diff = center - p;
-    auto costheta = sqrt(1 - radius * radius / dot(center_diff, center_diff));
-    auto solid_angle = 2 * PI * (1 - costheta);
-    return 1 / solid_angle;
-  }
-  vec3 random(const point3 &p) const override {
-    vec3 dir = center - p;
-    auto dist_sqr = dot(dir, dir);
-    Onb uvw;
-    uvw.build_from_w(dir);
-    return uvw.local(random_to_sphere(radius, dist_sqr));
   }
 };
 
